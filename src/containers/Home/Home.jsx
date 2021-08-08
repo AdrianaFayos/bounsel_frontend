4a32b1f3f9bdee6a6eb4946d5d94c074fdf8c9ab
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-08 09:02:25 +0200

    home container updated

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 import './Home.css'
 
-
 const Home = () => {
-    
+
     return (
         <div>
-            SOY HOME
+            <div className="homeContainer">
+
+                </div>
+                    <div className="homeContainer1">
+                   <Header />
+                   <div className="homeContent">
+                       <p className="text1">All the movies you want, and much more.</p>
+                   </div>
+                </div>
         </div>
     )
 
