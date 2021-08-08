3b3c296a52699bfe2eb72845056e869af26b3f48
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-08 09:55:14 +0200

    login working

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import './Home.css'
+import { connect } from "react-redux";
 
-const Home = () => {
+const Home = (props) => {
 
-    return (
-        <div>
-            <div className="homeContainer">
+
+    if(props.credentials?.user.id){
+        return(
+            <div>
+                <div className="homeContainer">
 
                 </div>
-                    <div className="homeContainer1">
-                   <Header />
-                   <div className="homeContent">
-                       <p className="text1">All the movies you want, and much more.</p>
-                   </div>
+                <div className="homeContainer1">
+                    <Header />
+                    <div className="homeContent">
+                        <p className="text1">SOY HOME CON USUARIO LOGIN</p>
+                    </div>
                 </div>
-        </div>
-    )
+            </div>
+            
+        )
+    } else {
+
+        return(
+    
+            <div>
+                <div className="homeContainer">
+
+                </div>
+                <div className="homeContainer1">
+                    <Header />
+                    <div className="homeContent">
+                        <p className="text1">All the movies you want, and much more.</p>
+                    </div>
+                </div>
+            </div>
+        )
+    
+    }    
 
 }
 
-export default Home;
+export default connect((state) => ({
+    credentials: state.credentials,
+  }))(Home);
