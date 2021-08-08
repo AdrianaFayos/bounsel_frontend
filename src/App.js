6021128a64e9f539da302d27bdccecfac6c4c66e
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-08 20:45:30 +0200

    start date and end date enpoint created at app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 import Home from './containers/Home/Home';
 import Login from './containers/Login/Login';
@@ -6,7 +11,40 @@ import Profile from './containers/Profile/Profile';
 import Records from './containers/Records/Records';
 import Register from './containers/Register/Register';
 
-function App() {
+function App (props) {
+
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  useEffect(() => {
+
+    if (props.credentials?.user.id){
+      const start = moment().format("YYYY-MM-DD HH:mm:ss");
+      setStartDate(start);
+    }
+
+  }, []);
+
+  useEffect(() => {
+  });
+
+
+  window.onunload = async function () {
+
+    if (props.credentials?.user.id){
+      const end = moment().format("YYYY-MM-DD HH:mm:ss");
+      setEndDate(end);
+  
+      let body = {
+        userId: props.credentials?.user.id,
+        startDate: startDate,
+        endDate : endDate,
+      };
+  
+      await axios.post('http://localhost:3006/recordsystem', body);
+  }
+}
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -29,4 +67,8 @@ function App() {
   );
 }
 
-export default App;
+export default connect((state) => ({
+
+  credentials:state.credentials
+
+}))(App);
