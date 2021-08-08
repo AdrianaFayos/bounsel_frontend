36f488f02138707a254f8b40b44d798db6228ba3
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-08 21:03:49 +0200

    records by date container updated

--- src/containers/Records/Records.jsx
+++ src/containers/Records/Records.jsx
@@ -1,22 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import './Records.css'
 import { connect } from "react-redux";
+import axios from 'axios';
 
 const Records = (props) => {
 
+    //hooks 
+    const [ records, setRecords] = useState({
+     startDate : "",
+     endDate : ""
+    });
+
+    const [ foundRecords, setFoundRecords] = useState([]);
+
+    useEffect(() => {
+        
+    },[]);
+
+    useEffect(() => {
+        searchRecords();
+    });
+
+    //Handlers
+    const updateDates = (e) => {
+      setRecords({ ...records, [e.target.name]: e.target.value });
+    };
+
+    const searchRecords = async () => {
+
+      let body = {
+        userId: props.credentials?.user.id,
+        startDate: records.startDate,
+        endDate : records.endDate,
+      };
+        
+      console.log(body);
+
+      let res = await axios.post('http://localhost:3006/recordsystem/bydate', body);
+  
+      setFoundRecords(res.data.results)
+      
+  } 
 
     if(props.credentials?.user.id) {
         return(
 
-            <div className="profileContainer">
+            <div className="recordContainer">
           
                 <Header/>
+
+                <div className="inputDates">
+                  <p>FROM:</p>
+                  <input type="date" title="startDate" name="startDate" onChange={updateDates} />
+                  <p>TO:</p>
+                  <input type="date" title="endDate" name="endDate" onChange={updateDates} />
+                </div>
     
-                <div className="userInfo">
-                  <p>NAME : {props.credentials?.user.name} </p>
-                  <p>LASTNAME : {props.credentials?.user.lastname} </p>
-                  <p>EMAIL : {props.credentials?.user.email} </p>
+                <div className="allRecords">
+                   {foundRecords.map((record, index) => (
+           
+                      <div key={index} className="recordFound">
+                        <p>{record.startDate}</p>
+                        <p>{record.endDate}</p>
+                      </div>
+                   ))}
                 </div>
    
             </div>
