4fff46abfae0927cd559edb34cacfa42763a4e9a
AdrianaFayos <adrifayosperis@gmail.com>
2021-08-08 09:02:50 +0200

    redux installed and created files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './containers/Home/Home';
 import Login from './containers/Login/Login';
 import Profile from './containers/Profile/Profile';
+import Register from './containers/Register/Register';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/login" exact component={Login}/>
           <Route path="/profile" exact component={Profile}/>
+          <Route path="/register" exact component={Register}/>
 
         </Switch>
         
